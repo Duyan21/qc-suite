50387900fc3b44e4c4c2cb1100c969464facfa03
Noah Ho <hoduy.ann@gmail.com>
2026-07-31 21:44:57 +0700

    feat: add Release admin CRUD API

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from routers.auth import router as auth_router
 from routers.projects import router as projects_router
+from routers.releases import router as releases_router
 from routers.requirements import router as requirements_router
 from routers.test_runs import router as test_runs_router
 from routers.test_cases import router as test_cases_router
@@ -11,6 +12,7 @@ app = FastAPI(title="QC Suite API")
 
 app.include_router(auth_router)
 app.include_router(projects_router)
+app.include_router(releases_router)
 app.include_router(requirements_router)
 app.include_router(test_runs_router)
 app.include_router(test_cases_router)
